f97e841b2fdf41fdfce5d6fb37b3fa5a68bd306e
root <fazleh2010@gmail.com>
2021-05-18 18:22:44 +0200

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -346,7 +346,8 @@ public class Main implements NullInterestingness,PredictionRules {
         ));*/
        
         List<String> predictKBGivenLInguistic = new ArrayList<String>(Arrays.asList(
-                predict_p_for_o_given_localized_l 
+                predict_s_for_o_given_l
+                //predict_p_for_o_given_localized_l 
                 //predict_p_for_s_given_localized_l
                 /*predict_p_for_o_given_l,
                 predict_p_for_s_given_l,
