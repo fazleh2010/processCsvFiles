fb78d67d780b9423406cec66d5b9fa7748a72868
root <fazleh2010@gmail.com>
2021-05-19 10:51:41 +0200

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -286,10 +286,14 @@ public class Main implements NullInterestingness,PredictionRules {
         ));*/
        
         List<String> predictKBGivenLInguistic = new ArrayList<String>(Arrays.asList(
-                //predict_s_for_o_given_l
+                predict_s_for_o_given_l,
+                predict_o_for_s_given_l,
+                predict_po_for_s_given_l,
+                predict_sp_for_o_given_l,
+                predict_sp_for_o_given_localized_l
                 //predict_p_for_o_given_localized_l 
                 //predict_p_for_s_given_localized_l
-                predict_p_for_o_given_l
+                //predict_p_for_o_given_l
                 /*
                 predict_p_for_s_given_l,
                 predict_sp_for_o_given_localized_l,
@@ -304,11 +308,11 @@ public class Main implements NullInterestingness,PredictionRules {
         
         List<String> interestingness = new ArrayList<String>();
         interestingness.add(AllConf);
-        //interestingness.add(Coherence);
-        //interestingness.add(Cosine);
-        //interestingness.add(Kulczynski);
-        //interestingness.add(IR);
-        //interestingness.add(MaxConf);
+        interestingness.add(Coherence);
+        interestingness.add(Cosine);
+        interestingness.add(Kulczynski);
+        interestingness.add(IR);
+        interestingness.add(MaxConf);
         for (String prediction : predictKBGivenLInguistic) {
             //String inputDir = baseDir + prediction + "/" ;
              String inputDir = baseDir + "/" ;
@@ -318,7 +322,7 @@ public class Main implements NullInterestingness,PredictionRules {
                 FileFolderUtils.createDirectory(outputDir);
                 Main ProcessFile = new Main(inputDir, outputDir, prediction, inter, LOGGER, ".csv", creationType);
                 //System.out.println(outputDir);
-                //CreateTXT.resultStrTxt(posTag,outputDir,txtDir, prediction, lemmatizer, inter);
+                CreateTXT.resultStrTxt(posTag,outputDir,txtDir, prediction, lemmatizer, inter);
             }
         }
         
